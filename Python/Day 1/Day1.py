a4abf85e3395880d51af0a6e13247cc70cfb207f
Ivan Imbert <ivan.imbert@epita.fr>
2021-12-01 07:55:46 +0100

    Day 1 (done in Python)

---
+++ Python/Day 1/Day1.py
@@ -0,0 +1,39 @@
+
+
+def GetIncrease(fileName) :
+    file = open(fileName, 'r')
+    lines = file.readlines()
+    prev = 100000
+    nbincrease = 0
+    for line in lines:
+        curr = int(line)
+        if prev < curr :
+            nbincrease+=1
+        prev = curr
+    return nbincrease
+
+print(GetIncrease("../../Data/Day 1/input.txt"))
+
+
+
+
+def GetGeneralIncreast(fileName) :
+    file = open(fileName, 'r')
+    lines = file.readlines()
+    curr_1 = 10000
+    curr_2 = 10000
+    curr_3 = 10000
+
+
+    nbincrease = 0
+    for line in lines:
+        curr = int(line)
+        if curr_1 + curr_2 + curr_3 < curr + curr_1 + curr_2:
+            nbincrease+=1
+        curr_3 = curr_2
+        curr_2 = curr_1
+        curr_1 = curr
+    return nbincrease
+
+
+print(GetGeneralIncreast("../../Data/Day 1/input.txt"))
