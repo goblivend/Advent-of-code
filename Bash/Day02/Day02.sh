b620f292065d36d5b87dea3e6966a09fbfba1580
Ivan Imbert <ivan.imbert@epita.fr>
2021-12-02 20:01:34 +0100

    renamed stuff

---
+++ Bash/Day02/Day02.sh
@@ -0,0 +1,22 @@
+depth=0 ; forward=0 ; depthaim=0
+while IFS=$"" read -r line; do 
+    IFS=$' '; data=($line); unset IFS
+
+    [ ${#data[@]} -ne 2 ] && [ echo "${#data[@]} for $data" continue ]
+
+#    if [ ${#data[@]} -ne 2 ] ; then 
+#        echo "${#data[@]} for $data" ; continue
+#    else
+    case $data[0] in
+        "up"*)
+            depth=$(($((depth))-$((data[1])))) ;;
+        "down"*)
+            depth=$(($((depth))+$((data[1])))) ;;
+        "forward"*)
+            forward=$(($((forward))+$((data[1]))))
+            depthaim=$(($((depthaim))+$((depth))*$((data[1])))) ;;
+        *) 
+        echo "no found data '${data[0]}'" ;;
+    esac ; done <"$1"
+
+echo "Part 1 : $depth * $forward" && echo "Part 2 : $depthaim * $forward"
