a4041dee812d9930b0e0abcc68a7ca0afbea5876
Ivan Imbert <ivan.imbert@epita.fr>
2021-12-02 07:25:14 +0100

    removing Subjects and Day2 Python

---
+++ Python/Day2/Day2.py
@@ -0,0 +1,52 @@
+inputFile = "../../Data/Day2/input.txt"
+exampleFile  = "../../Data/Day2/ExampleData.txt"
+
+def GetIncrease(fileName) :
+    file = open(fileName, 'r')
+    lines = file.readlines()
+    horizontal = 0
+    depth = 0
+    for line in lines:
+        data = line.split(' ')
+        if len(data) !=2 :
+            print(line)
+        else :
+            data[1].strip()
+            if data[0] == "down" :
+                depth += int(data[1])
+            elif data[0] == "up" :
+                depth -= int(data[1])
+            elif data[0] == "forward" :
+                horizontal += int(data[1])
+            else :
+                print(line)
+    print((horizontal, depth))
+    return horizontal*depth
+
+print(GetIncrease(exampleFile))
+
+def GetIncreaseSecond(fileName) :
+    file = open(fileName, 'r')
+    lines = file.readlines()
+    horizontal = 0
+    depth = 0
+    aim = 0
+    for line in lines:
+        data = line.split(' ')
+        if len(data) !=2 :
+            print(line)
+        else :
+            data[1].strip()
+            if data[0] == "down" :
+                aim += int(data[1])
+            elif data[0] == "up" :
+                aim -= int(data[1])
+            elif data[0] == "forward" :
+                horizontal += int(data[1])
+                depth += aim*int(data[1])
+            else :
+                print(line)
+    print((horizontal, depth))
+    return horizontal*depth
+
+print(GetIncreaseSecond(exampleFile))
