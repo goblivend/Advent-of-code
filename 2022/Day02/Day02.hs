952670caf78f32396a7c775762bbe923d3242453
Ivan Imbert <ivan.imbert@epita.fr>
2022-12-02 17:26:53 +0100

    feat: Ugly V2

--- 2022/Day02/Day02.hs
+++ 2022/Day02/Day02.hs
@@ -13,11 +13,15 @@ getPlays :: [String] -> [(Char, Char)]
 getPlays (e:l) = (e !! 0, e !! 2) : getPlays l
 getPlays [] = []
 
-getResults :: [(Int, Int)] -> Int
-getResults [] = 0
-getResults ((a, b):l) =b + (getResults l) + if a == b then 3
+getResults :: (Int, Int) -> Int
+getResults (a, b) = b + if a == b then 3
                         else if (mod (b-1)  3) == a || (mod (a+1)  3) == b  then 6
                         else 0
+
+cleanInputs :: Int -> Char -> (Int, Int)
+cleanInputs a b = if (b == 'X') then if a-1 == 0 then (a, 3) else (a, a-1)
+                        else if (b == 'Y') then (a, a)
+                        else if a+1 == 4 then (a, 1) else (a,  a+1)
 
 
 
@@ -26,4 +30,8 @@ main = do
     content <- readFile "input.txt"
     let plays =  map (\(a,b) -> (oponentPoints a, inputPoints b)) $ getPlays $ lines content
     putStrLn $ id "Following the first rules, the final score is :"
-    print $  getResults plays
+    print $ sum $ map (\(a,b) -> getResults(a, b)) $  plays
+
+    let plays2 = map (\(a, b) -> (cleanInputs (oponentPoints a) b)) $ getPlays $ lines content
+    putStrLn $ id "Following the second rules, the final score is :"
+    print $ sum $  map (\(a,b) -> getResults(a, b)) $  plays2
