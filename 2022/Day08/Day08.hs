dc95d432aadc06a2e229e2b0e4412914f808f75a
Ivan Imbert <ivan.imbert@epita.fr>
2022-12-08 14:52:28 +0100

    feat: Day08 Haskell part1

--- 2022/Day08/Day08.hs
+++ 2022/Day08/Day08.hs
@@ -6,34 +6,26 @@ import Data.List.Split
 import Data.Containers.ListUtils
 import Debug.Trace
 
-getVisible :: [[(Int, Bool)]] -> Int -> Int -> Bool
-getVisible input x y
-    | x == 0 || y == 0
-             || x == (length input)-1
-             || y == (length $ input !! 0)-1 = True
-    | snd (input !! x !! y) = True
-    | otherwise = x > 0                      && snd (input !! (x-1) !! y) && (input !! (x-1) !! y) < (input !! x !! y)
-            || x < (length input)            && snd (input !! (x+1) !! y) && (input !! (x+1) !! y) < (input !! x !! y)
-            || y > 0                         && snd (input !! x !! (y-1)) && (input !! x !! (y-1)) < (input !! x !! y)
-            || (y+1) < (length $ input !! 0) && snd (input !! x !! (y+1)) && (input !! x !! (y+1)) < (input !! x !! y)
+checkVisibility :: [[Int]] -> Int -> Int -> Bool
+checkVisibility input x y = 
+                            all (\e -> e) (map (\x2 -> input !! y !! x > input !! y !! x2) [x+1..(length $ input !! 0) - 1])
+                         || all (\e -> e) (map (\x2 -> input !! y !! x > input !! y !! x2) [0..x-1])
+                         || all (\e -> e) (map (\y2 -> input !! y !! x > input !! y2 !! x) [0..y-1])
+                         || all (\e -> e) (map (\y2 -> input !! y !! x > input !! y2 !! x) [y+1..(length input) - 1])
 
-changeVisibility :: [[(Int, Bool)]] -> Int -> Int -> Bool -> [[(Int, Bool)]]
-changeVisibility input x y v = take x input ++ [take y (input !! x) ++ [(fst (input !! x !! y), v)] ++ drop (y+1) (input !! x)] ++ drop (x+1) input
+--changeVisibility :: [[(Int, Bool)]] -> Int -> Int -> Bool -> [[(Int, Bool)]]
+--changeVisibility input x y v = take x input ++ [take y (input !! x) ++ [(fst (input !! x !! y), v)] ++ drop (y+1) (input !! x)] ++ drop (x+1) input
 
-checkVisibility :: [[(Int, Bool)]] -> Int -> Int -> [[(Int, Bool)]]
-checkVisibility input x y
-    | y >= (length $ input !! 0) = input
-    | otherwise = checkVisibility (changeVisibility input x y (getVisible input x y)) (mod (x+1) $ length input) (y + floor ((fromIntegral $ x+1) / (fromIntegral $ length input)))
 
-doNChecks :: [[(Int, Bool)]] -> Int -> [[(Int, Bool)]]
-doNChecks input 0 = input
-doNChecks input n = doNChecks (checkVisibility input 0 0) (n-1)
 
 
 main :: IO ()
 main = do
     content <- readFile "input.txt"
-    let input = map (\line ->  map (\x -> (read x :: Int, False)) $ chunksOf 1 line) $ lines content
-    --print $ checkVisibility input 1 0
-    let visibility = doNChecks input 100
-    print $ sum $ map (\x -> if snd x then 1 else 0) $ concat visibility
+    let input = map (\line ->  map (\x -> read x :: Int) $ chunksOf 1 line) $ lines content
+    let visibility = map (\y -> map (\x -> checkVisibility input x y) [0..(length (input !! y)) - 1]) [0..(length input) - 1]
+    --print visibility
+    --let range = [2+1..(length (input !! 1)) - 1]
+    --print $ all (\e -> e) $ map (\x -> (x > input !! 1 !! x)) range --  checkBigger 5 input 2 1 (\x -> x+1) (\y -> y) 
+
+    print $ sum $ map (\e -> if e then 1 else 0) $ concat visibility
