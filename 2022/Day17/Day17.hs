3a5460b4473c27f8583153d741e4051bd42f4eb2
Ivan Imbert <ivan.imbert@epita.fr>
2022-12-18 14:35:44 +0100

    feat: try Day17 Haskell part2

--- 2022/Day17/Day17.hs
+++ 2022/Day17/Day17.hs
@@ -28,8 +28,9 @@ instance Read Rock where
     readsPrec _ ('@':r) = [(Fall, r)]
     readsPrec _ ('#':r) = [(Fixed, r)]
 
-rocks = map (map (map (\r -> read [r] :: Rock)) . lines) ["..@@@@.", "...@...\n..@@@..\n...@...", "....@..\n....@..\n..@@@..", "..@....\n..@....\n..@....\n..@....", "..@@...\n..@@..."]
-emptyLine = (map (\r -> read [r] :: Rock)) "......."
+rocks = cycle $ map (map (map (\r -> read [r] :: Rock)) . lines) ["..@@@@.", "...@...\n..@@@..\n...@...", "....@..\n....@..\n..@@@..", "..@....\n..@....\n..@....\n..@....", "..@@...\n..@@..."]
+emptyLine =  (map (\r -> read [r] :: Rock)) "......."
+emptyLines =  replicate 3 emptyLine
 
 cleanMap :: [[Rock]] -> [[Rock]]
 cleanMap [] = []
@@ -43,8 +44,8 @@ lock mp = map (map (\r -> if r == Fall then Fixed else r)) mp
 isValid :: [[Rock]] -> Int -> Int -> Bool
 isValid mp x y = 0 <= x && x < 7 && y < length mp && mp !! y !! x /= Fixed
 
-insertRock :: Int -> [[Rock]] -> [[Rock]]
-insertRock n mp = (rocks !! (n `mod` (length rocks))) ++ replicate 3 emptyLine ++ cleanMap mp
+insertRock :: [[Rock]] -> [[Rock]] -> [[Rock]]
+insertRock r mp = r ++ emptyLines ++ cleanMap mp
 
 getDir :: Jet -> Int
 getDir j
@@ -58,13 +59,13 @@ replaceXY :: Int -> Int -> a -> [[a]] -> [[a]]
 replaceXY x y val l = replaceAt y (replaceAt x val (l !! y)) l
 
 getYFall :: [[Rock]] -> [Int]
-getYFall mp = filter (\y -> elem Fall (mp !! y)) [0..length mp - 1]
+getYFall mp = findIndices (\l -> elem Fall l) mp
 
 isAllValid :: [[Rock]] -> Int -> Int -> Bool
 isAllValid mp dx dy = all (\(x, y) -> isValid mp (x+dx) (y+dy)) co
     where
         ys = getYFall mp
-        co = concat $ map (\y -> map (\x -> (x, y)) $ elemIndices Fall (mp !! y))  ys
+        co = concatMap (\y -> map (\x -> (x, y)) $ elemIndices Fall (mp !! y))  ys
 
 fall :: Int -> Int -> Int -> Int -> [[Rock]] -> [[Rock]]
 fall x y dx dy mp = replaceXY (x+dx) (y+dy) Fall $ replaceXY x y Void mp
@@ -83,12 +84,12 @@ fallOneLine mp dx dy= newMp
         sortMe = if dx == -1 then sort else reverse . sort
         newMp = foldl (fallLine dx dy sortMe) mp ys
 
-makeFallOne :: [Jet] -> Int -> [[Rock]] -> (Int, [[Rock]])
-makeFallOne jets nbJet mp = (newNbJet, newMp)
+makeFallOne :: [Jet] -> [[Rock]] -> ([Jet], [[Rock]])
+makeFallOne jets mp = (newJets, newMp)
     where
-        dir = (getDir $ jets !! nbJet)
+        dir = getDir $ head jets
         sideMp = if isAllValid mp dir 0 then fallOneLine mp dir 0 else mp
-        (newNbJet, newMp) = if not $ isAllValid sideMp 0 1 then (nbJet+1, lock sideMp) else makeFallOne jets (nbJet+1) $ fallOneLine sideMp 0 1
+        (newJets, newMp) = if not $ isAllValid sideMp 0 1 then (tail jets, lock sideMp) else makeFallOne (tail jets) $ fallOneLine sideMp 0 1
 
 shorttenMp :: [[Rock]] -> [Bool] -> [Bool] -> (Int, [[Rock]])
 shorttenMp [] _ _ = (0, [])
@@ -98,31 +99,28 @@ shorttenMp (l:mp) prevSeen seen
         where
             (ln, newMp) = shorttenMp mp seen (map (==Fixed) l)
 
+emptySeen = replicate 7 False
 
-makeFallAll :: Int -> [Jet] -> Int -> Int -> [[Rock]] -> Int
-makeFallAll nbRocks jets nbJet currRock mp
-    | nbRocks <= currRock = length mp
+makeFallAll :: Int -> [Jet] -> [[[Rock]]] -> [[Rock]] -> Int
+makeFallAll nbRocks jets rocks mp
+    | nbRocks <= 0 = length mp
     | otherwise = len
     where
-        insertedMp = {-trace ("Inserting " ++ show currRock)-} (insertRock currRock mp)
-        (newNbJet, fallenMp) = {-trace ("Made one fall, jets at : "++ show nbJet)-} (makeFallOne jets nbJet insertedMp)
-        (cleanLen, cleanMp) = {-trace ("Cleaning")-} shorttenMp (cleanMap fallenMp) (replicate 7 False) (replicate 7 False)
-
-        len = cleanLen + makeFallAll nbRocks jets newNbJet (currRock + 1) (trace (show (length mp)) (cleanMp))
-
+        insertedMp = insertRock (head rocks) mp
+        (newJets, fallenMp) =  makeFallOne jets insertedMp
+        (cleanLen, cleanMp) =  shorttenMp (cleanMap fallenMp) emptySeen emptySeen
+        len = cleanLen + makeFallAll (nbRocks-1) newJets  (tail rocks) cleanMp
 
 main :: IO ()
 main = do
-    content <- readFile "input.txt"
-    let jets = map (\j -> read [j] :: Jet) . head $ lines content
-    let infJets = concat . repeat $ jets
-
-    -- let (endN, newMp) = makeFallOne jets 0 mp
-    -- pretty newMp
-    -- let (endN2, newMp2) = makeFallOne jets (endN+4) $ insertRock 2 newMp
-    -- pretty newMp2
-    print $  makeFallAll 10000 infJets 0 0 []
+    content <- readFile "input2.txt"
+    -- let jets = cycle $ map (\j -> read [j] :: Jet) . head $ lines content
+    print $ length . map (\j -> read [j] :: Jet) . head $ lines content
+    -- print $  makeFallAll 100 jets rocks []
+    -- print $  makeFallAll 1000 jets rocks []
+    -- print $  makeFallAll 10000 jets rocks []
+    -- print $  makeFallAll 100000 jets rocks []
+    -- print $  makeFallAll 500000 jets rocks []
     -- print $  makeFallAll 1000000000000 infJets 0 0 []
-    -- print $ length $ makeFallAll 2022 infJets 0 0 []
     where
         pretty mp = putStrLn $ unlines $ map (concat . map show ) mp
