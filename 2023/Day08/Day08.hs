6f292a12d9cd0cc2f8656fab2eb359ea9ea1415f
Ivan Imbert <ivan.imbert@epita.fr>
2023-12-07 00:28:55 +0100

    refacto(2023/Day08): Cleaned up code

--- 2023/Day08/Day08.hs
+++ 2023/Day08/Day08.hs
@@ -10,11 +10,6 @@ import Debug.Trace(trace)
 
 data Input = Input {intrs:: [((String, String) -> String)], mps :: Map String (String, String)}
 
-instance Show Input where
-    show (Input _ mps) = show mps
-
-
-
 parseInput :: String -> Input
 parseInput s = Input instructions mps
     where
@@ -25,7 +20,6 @@ parseInput s = Input instructions mps
         readTuple = (\[e1, e2] -> (drop 1 e1, take 3 e2)) . splitOn ", "
         mps = fromList . map ((\[e1, e2] -> (e1, readTuple e2)) . splitOn " = ") $ drop 2 lined
 
-
 part1 :: Input -> String -> Int
 part1 (Input instr mps) starting = recurse starting $ cycle instr
     where
@@ -33,25 +27,18 @@ part1 (Input instr mps) starting = recurse starting $ cycle instr
         recurse s (e:l) = 1 + recurse (e (mps ! s)) l
 
 part2 :: Input -> (String -> Bool) -> Int
-part2 (Input instr mps) starting = product te
+part2 (Input instr mps) starting = product . uniq . sort $ concatMap (multiplicators primes . findZ (cycle instr) 0) (filter starting $ keys mps)
     where
-        findZ :: [((String, String) -> String)] -> Int -> String -> Int
         findZ (e:l) i s
             | "Z" `isSuffixOf` s = i
-            | otherwise         =  next
-                where
-                    next = findZ l (i+1) ((e . (mps !)) s)
-        res =  map (findZ (cycle instr) 0) (filter starting $ keys mps)
+            | otherwise          = findZ l (i+1) (e (mps ! s))
         primes = sieve [2..]
             where
-                sieve (p:xs) = p : sieve [x|x <- xs, x `mod` p > 0]
-        multiplicators :: [Int] -> Int -> [Int]
+                sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
         multiplicators (e:l) n
             | e > n =  []
             | n `mod` e == 0 = e : multiplicators (e:l) (div n e)
             | otherwise =  (multiplicators l n)
-
-        te = uniq . sort . concat $ map (multiplicators primes) res
 
 
 
